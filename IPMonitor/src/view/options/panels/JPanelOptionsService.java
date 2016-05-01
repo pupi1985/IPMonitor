6ae320a596bd1aa832fdfd10a4e45069778001d6
pupi1985 <pupi1985@bitbucket.org>
2016-05-01 20:07:23 -0300

    Reformat JPanelOptionsService

--- IPMonitor/src/view/options/panels/JPanelOptionsService.java
+++ IPMonitor/src/view/options/panels/JPanelOptionsService.java
@@ -72,35 +72,35 @@ public class JPanelOptionsService extends JPanel {
 		GroupLayout jPanelTempLayout = new GroupLayout(jPanelTemp);
 		jPanelTemp.setLayout(jPanelTempLayout);
 		jPanelTempLayout.setHorizontalGroup(jPanelTempLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-				.addGroup(jPanelTempLayout.createSequentialGroup().addContainerGap().addGroup(jPanelTempLayout
-						.createParallelGroup(GroupLayout.Alignment.TRAILING, false)
-						.addComponent(jButtonStart, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addComponent(jButtonInstall, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE,
-								Short.MAX_VALUE)
-						.addComponent(jButtonTest, GroupLayout.Alignment.LEADING, GroupLayout.DEFAULT_SIZE,
-								GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-						.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-						.addGroup(jPanelTempLayout.createParallelGroup(GroupLayout.Alignment.TRAILING)
-								.addComponent(jButtonStop, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE,
-										Short.MAX_VALUE)
-								.addComponent(jButtonUninstall, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE,
-										Short.MAX_VALUE))
-						.addContainerGap()));
+			.addGroup(jPanelTempLayout.createSequentialGroup()
+		        .addContainerGap()
+		        .addGroup(jPanelTempLayout.createParallelGroup(GroupLayout.Alignment.TRAILING, false)
+					.addComponent(jButtonStart, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addComponent(jButtonInstall, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addComponent(jButtonTest, GroupLayout.Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+			.addGroup(jPanelTempLayout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+				.addComponent(jButtonStop, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+				.addComponent(jButtonUninstall, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+			.addContainerGap()));
 
 		jPanelTempLayout.linkSize(SwingConstants.HORIZONTAL, new Component[] {
 				jButtonInstall, jButtonStart, jButtonStop, jButtonTest, jButtonUninstall
 		});
 
 		jPanelTempLayout.setVerticalGroup(jPanelTempLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-				.addGroup(jPanelTempLayout.createSequentialGroup().addContainerGap()
-						.addGroup(jPanelTempLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-								.addComponent(jButtonInstall).addComponent(jButtonUninstall))
-						.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-						.addGroup(jPanelTempLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-								.addComponent(jButtonStart).addComponent(jButtonStop))
-						.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED).addGroup(jPanelTempLayout
-								.createParallelGroup(GroupLayout.Alignment.BASELINE).addComponent(jButtonTest))
-						.addContainerGap()));
+			.addGroup(jPanelTempLayout.createSequentialGroup().addContainerGap()
+				.addGroup(jPanelTempLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+					.addComponent(jButtonInstall)
+					.addComponent(jButtonUninstall))
+				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+				.addGroup(jPanelTempLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+					.addComponent(jButtonStart)
+					.addComponent(jButtonStop))
+				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+				.addGroup(jPanelTempLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+				    .addComponent(jButtonTest))
+				.addContainerGap()));
 
 		jPanelTempLayout.linkSize(SwingConstants.VERTICAL, new Component[] {
 				jButtonInstall, jButtonStart, jButtonStop, jButtonTest, jButtonUninstall
